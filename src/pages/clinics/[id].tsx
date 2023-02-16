4246d0e54da84311f1f9a66e32d9341579422dd8
Kenan Agaev <kenkosh@yandex.ru>
2023-02-16 22:55:55 +0300

    adds getStaticPaths

--- src/pages/clinics/[id].tsx
+++ src/pages/clinics/[id].tsx
@@ -1,8 +1,24 @@
-import { InferGetStaticPropsType } from 'next';
+import { AxiosResponse } from 'axios';
+import { GetStaticPaths, InferGetStaticPropsType } from 'next';
 import { ContainerComponent, Layout } from '@/components';
 import { wrapper } from '@/stores';
 import { getSiteSettings } from '@/stores/api';
 import getRunningGlobalQueries from '@/stores/api/global.api';
+import { axiosClient } from '@/stores/assets';
+import { IClinic } from '@/shared/types';
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const response = await axiosClient.get<AxiosResponse<IClinic[]>>('/clinics', {
+    params: { fields: 'id' },
+  });
+
+  return {
+    paths: response.data.data.map(clinic => ({
+      params: { id: clinic.id.toString() },
+    })),
+    fallback: false,
+  };
+};
 
 export const getStaticProps = wrapper.getStaticProps(store => async () => {
   const siteSettings = await store.dispatch(getSiteSettings.initiate());
--- src/pages/doctors/[id].tsx
+++ src/pages/doctors/[id].tsx
@@ -1,8 +1,24 @@
-import { InferGetStaticPropsType } from 'next';
+import { AxiosResponse } from 'axios';
+import { GetStaticPaths, InferGetStaticPropsType } from 'next';
 import { ContainerComponent, Layout } from '@/components';
 import { wrapper } from '@/stores';
 import { getSiteSettings } from '@/stores/api';
 import getRunningGlobalQueries from '@/stores/api/global.api';
+import { IDoctor } from '@/shared/types';
+import { axiosClient } from '@/stores/assets';
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const response = await axiosClient.get<AxiosResponse<IDoctor[]>>('/doctors', {
+    params: { fields: 'id' },
+  });
+
+  return {
+    paths: response.data.data.map(doc => ({
+      params: { id: doc.id.toString() },
+    })),
+    fallback: false,
+  };
+};
 
 export const getStaticProps = wrapper.getStaticProps(store => async () => {
   const siteSettings = await store.dispatch(getSiteSettings.initiate());
